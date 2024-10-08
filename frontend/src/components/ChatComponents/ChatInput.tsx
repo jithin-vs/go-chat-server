12291116a835551e96c3cafd8756d3a9b7994fc5
Jithin.s <jithinvs1045@gmail.com>
2024-10-08 18:51:00 +0530

    input handler added

--- frontend/src/components/ChatComponents/ChatInput.tsx
+++ frontend/src/components/ChatComponents/ChatInput.tsx
@@ -1,6 +1,34 @@
+"use client"
+
+import axios from 'axios';
+import { FormEvent, useRef } from 'react';
 function ChatInput() {
+   
+    const messageRef = useRef<HTMLTextAreaElement | null>(null);
+
+    const handleSubmit = async(e: React.FormEvent<HTMLFormElement>) => { 
+        e.preventDefault();
+        const message = messageRef.current?.value;
+        console.log(message);
+        if (!message) {
+            console.log("no message entered");
+        }
+        try {
+            const response = await axios.post('/api/messages', { message });
+            if (response.status === 200) {
+                console.log("message sent successfully");
+                if (messageRef.current) {
+                    messageRef.current.value = '';  
+                }
+                
+            }
+        } catch (error) {
+            
+        }
+    };
+
   return (
-    <form className="mx-8">
+    <form onSubmit={handleSubmit} className="mx-8">
     <label htmlFor="chat" className="sr-only">Your message</label>
     <div className="flex items-center px-3 py-2 rounded-lg bg-gray-50 dark:bg-gray-700">
         <button
@@ -28,6 +56,7 @@ function ChatInput() {
             rows={1}
             className="block mx-4 p-2.5 w-full text-sm text-gray-900 bg-white rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             placeholder="Your message..."
+            ref={messageRef}      
         ></textarea>
         <button
             type="submit"
